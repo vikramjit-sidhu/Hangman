368633791af2562f8184ef69fa4d49d4b9180858
S <s>
2015-08-11 21:31:56 +0530

    {"type":"TI", "runs":"0"}

--- Hangman.java
+++ Hangman.java
@@ -93,7 +93,7 @@ public class Hangman extends ConsoleProgram {
     			continue;
     		}
     		break;
-    		alphabetEntered = 
+    		alphabetEntered = convertToUpperCase(charEntered);
     	}
     	return alphabetEntered;
     }
