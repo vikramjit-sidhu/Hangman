940fd28565b443c1f0c8e404303ce930fda0b5d6
S <s>
2015-08-13 14:25:48 +0530

    {"type":"TI", "runs":"0"}

--- Hangman.java
+++ Hangman.java
@@ -45,7 +45,9 @@ public class Hangman extends ConsoleProgram {
     	String wordToGuess = getGameWord();
     	/* Setting the word left to guess as initially the whole word */
     	remainingWordToGuess = wordToGuess;
-    	
+    	/* call method to update the canvas */
+		canvas.displayWord(wordGuessedSoFar);
+		
     	/* The initial blank word that the user has to guess set to be blanks (eg. ---- ) */
     	String wordGuessedSoFar = generateGuessedWord(wordToGuess.length());
     	
