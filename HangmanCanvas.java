57d3e18ce6df6d916dad520bb0b5440ec964619f
S <s>
2015-08-13 14:33:11 +0530

    {"type":"TI", "runs":"0"}

--- HangmanCanvas.java
+++ HangmanCanvas.java
@@ -31,7 +31,7 @@ public class HangmanCanvas extends GCanvas {
 		drawRope();
 		
 		/* Set up the labels which keep track of the incorrect guesses and word so far */
-		
+		addIncorrectGuessLabel();
 	}
 
 /**
