d9d8f1255f21faf27d9eaf7c642f8f6c618847d1
S <s>
2015-08-13 13:52:56 +0530

    {"type":"TI", "runs":"0"}

--- HangmanCanvas.java
+++ HangmanCanvas.java
@@ -8,6 +8,19 @@ import acm.graphics.*;
 
 public class HangmanCanvas extends GCanvas {
 
+/* Constants for the simple version of the picture (in pixels) */
+	private static final int SCAFFOLD_HEIGHT = 360;
+	private static final int BEAM_LENGTH = 144;
+	private static final int ROPE_LENGTH = 18;
+	private static final int HEAD_RADIUS = 36;
+	private static final int BODY_LENGTH = 144;
+	private static final int ARM_OFFSET_FROM_HEAD = 28;
+	private static final int UPPER_ARM_LENGTH = 72;
+	private static final int LOWER_ARM_LENGTH = 44;
+	private static final int HIP_WIDTH = 36;
+	private static final int LEG_LENGTH = 108;
+	private static final int FOOT_LENGTH = 28;
+	
 /** Resets the display so that only the scaffold appears */
 	public void reset() {
 		/* You fill this in */
@@ -32,17 +45,6 @@ public class HangmanCanvas extends GCanvas {
 		/* You fill this in */
 	}
 
-/* Constants for the simple version of the picture (in pixels) */
-	private static final int SCAFFOLD_HEIGHT = 360;
-	private static final int BEAM_LENGTH = 144;
-	private static final int ROPE_LENGTH = 18;
-	private static final int HEAD_RADIUS = 36;
-	private static final int BODY_LENGTH = 144;
-	private static final int ARM_OFFSET_FROM_HEAD = 28;
-	private static final int UPPER_ARM_LENGTH = 72;
-	private static final int LOWER_ARM_LENGTH = 44;
-	private static final int HIP_WIDTH = 36;
-	private static final int LEG_LENGTH = 108;
-	private static final int FOOT_LENGTH = 28;
+
 
 }
