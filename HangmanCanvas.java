66300fb363d5a49ecdd10f27233be5f9932ee49f
S <s>
2015-08-13 15:33:46 +0530

    {"type":"TI", "runs":"0"}

--- HangmanCanvas.java
+++ HangmanCanvas.java
@@ -23,6 +23,8 @@ public class HangmanCanvas extends GCanvas {
 	private static final int LEG_LENGTH = 108;
 	private static final int FOOT_LENGTH = 28;
 	
+/** The X offset for the labels which keep track of the incorrect guesses,
+ *  and the correct guesses made */	
 	private static final int LABEL_X_OFFSET = 50;
 	
 /** Resets the display so that only the scaffold appears */
