a5b03bb3ca15a2778d9cd15c67ea5a1013356993
S <s>
2015-08-13 14:40:26 +0530

    {"type":"TI", "runs":"0"}

--- HangmanCanvas.java
+++ HangmanCanvas.java
@@ -25,7 +25,9 @@ public class HangmanCanvas extends GCanvas {
 	public void reset() {
 		removeAll();
 		setWindowDimensions();
-		calculateScaffoldDimensions();
+		/* The scaffold is the only element whose co-ordinates are calculated, 
+		 * the co-ordinates of the remaining elements are calculated relative to it*/
+//		calculateScaffoldDimensions();
 		/* Draw the initial parts of the game */
 		drawScaffold();
 		drawBeam();
