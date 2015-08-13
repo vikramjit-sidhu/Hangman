55f2c88efa0d3e792a457874a1c60fbe01b1984f
S <s>
2015-08-13 14:40:39 +0530

    {"type":"TI", "runs":"0"}

--- HangmanCanvas.java
+++ HangmanCanvas.java
@@ -28,6 +28,7 @@ public class HangmanCanvas extends GCanvas {
 		/* The scaffold is the only element whose co-ordinates are calculated, 
 		 * the co-ordinates of the remaining elements are calculated relative to it*/
 //		calculateScaffoldDimensions();
+		
 		/* Draw the initial parts of the game */
 		drawScaffold();
 		drawBeam();
