ec19f29dc61278ecd80fe6d611b89f7169314246
S <s>
2015-08-14 15:05:40 +0530

    {"type":"TI", "runs":"0"}

--- HangmanCanvas.java
+++ HangmanCanvas.java
@@ -165,12 +165,12 @@ public class HangmanCanvas extends GCanvas {
 /** The label displaying the word which the user has guessed so far */
 	private GLabel wordGuessedSoFarLabel;
 	
-	private GPoint wordGuessedSoFarPoint;
+	private GPoint wordGuessedSoFarStartPoint;
 	
 /**	The label recording all the incorrect guesses made by the user */
 	private GLabel incorrectGuessesLabel;
 	
-	private GPoint wordGuessedSoFar;
+	private GPoint incorrectGuessesStartPoint;
 	
 /** The width and height of the application window, where the elements are drawn */	
 	private int windowWidth;
