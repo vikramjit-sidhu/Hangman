53a6af695a1d54df1223461da02320a5e96761aa
S <s>
2015-08-14 15:27:43 +0530

    {"type":"TI", "runs":"0"}

--- HangmanCanvas.java
+++ HangmanCanvas.java
@@ -160,7 +160,7 @@ public class HangmanCanvas extends GCanvas {
 	
 	private void setWordGuessedPoint(); {
 		double ypos = scaffoldTopY + SCAFFOLD_HEIGHT + Y_OFFSET_BETWEEN_SCAFFOLD_LABEL; 
-		incorrectGuessesStartPoint.setLocation(LABEL_X_OFFSET, );
+		incorrectGuessesStartPoint.setLocation(LABEL_X_OFFSET, ypos);
 	}
 	
 	private void addIncorrectGuessLabel() {
