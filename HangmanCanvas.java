333db312f532a983bc12331339a3b111628a1cc5
S <s>
2015-08-14 15:12:21 +0530

    {"type":"TI", "runs":"0"}

--- HangmanCanvas.java
+++ HangmanCanvas.java
@@ -153,7 +153,7 @@ public class HangmanCanvas extends GCanvas {
  *  re-calculating the point each time.	
  */
 	private void addWordGuessedLabel() {
-		wordGuessedSoFarLabel = new GLabel("", 50, 50);
+		wordGuessedSoFarLabel = new GLabel("", );
 		add(wordGuessedSoFarLabel);
 	}
 	
