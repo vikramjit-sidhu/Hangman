0b7f4356c2b9b0e1419d0062b31d29192edbf7b0
S <s>
2015-08-19 23:43:57 +0530

    {"type":"TI", "runs":"0"}

--- HangmanLexicon.java
+++ HangmanLexicon.java
@@ -57,19 +57,6 @@ public class HangmanLexicon {
 /** Returns the word at the specified index. */
 	public String getWord(int index) {
 		return wordList.get(index);
-		
-		switch (index) {
-			case 0: return "BUOY";
-			case 1: return "COMPUTER";
-			case 2: return "CONNOISSEUR";
-			case 3: return "DEHYDRATE";
-			case 4: return "FUZZY";
-			case 5: return "HUBBUB";
-			case 6: return "KEYHOLE";
-			case 7: return "QUAGMIRE";
-			case 8: return "SLITHER";
-			case 9: return "ZIRCON";
-			default: throw new ErrorException("getWord: Illegal index");
 		}
 	};
 	
