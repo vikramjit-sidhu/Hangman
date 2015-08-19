3ce1157365ed65ae080df36ca628235f4f31a3b1
S <s>
2015-08-19 13:15:32 +0530

    {"type":"TI", "runs":"0"}

--- HangmanCanvas.java
+++ HangmanCanvas.java
@@ -258,7 +258,7 @@ public class HangmanCanvas extends GCanvas {
 	private void drawLeftArm() {
 		GPoint bodyStartPt = getBodyStartPoint();
 		double xpos = bodyStartPt.getX();
-		double ypos = bodyStartPt.getX() + ARM_OFFSET_FROM_HEAD;
+		double ypos = bodyStartPt.getY() + ARM_OFFSET_FROM_HEAD;
 		/* drawing the upper arm (horizontal line) */
 		add(new GLine(xpos, ypos, xpos - UPPER_ARM_LENGTH, ypos));
 		/* drawing the lower arm (vertical line) */
